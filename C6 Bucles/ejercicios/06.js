206c3271643192e76042e61fcab932af00a7ee3c
Hiram Carreño <143430604+Abiff11@users.noreply.github.com>
2025-08-28 00:06:32 -0600

    Empezamos con bucles

--- C6 Bucles/ejercicios/06.js
+++ C6 Bucles/ejercicios/06.js
@@ -2,6 +2,13 @@ function tieneTresDigitos(num) {
   // Si el número recibido tiene tres dígitos, retorna true.
   // Caso contrario, retorna false.
   // Tu código:
+  if (num.toString().length === 3) {
+    return true;
+    
+  }
+  return false;
 }
+
+console.log(tieneTresDigitos(120));
 
 module.exports = tieneTresDigitos;
--- C6 Bucles/ejercicios/08.js
+++ C6 Bucles/ejercicios/08.js
@@ -3,6 +3,7 @@ function esPositivoOInferiorA10(a) {
    // Retorna true si es positivo y menor que 10.
    // Retorna false en caso contrario.
    // Tu código:
+   
 }
 
 module.exports = esPositivoOInferiorA10;
