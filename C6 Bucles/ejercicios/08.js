ed55204caaf2c8060eeebbcbb273e74cd58602ed
Hiram Carreño <143430604+Abiff11@users.noreply.github.com>
2025-09-01 01:03:50 -0600

    Avanzamos con bucles

--- C6 Bucles/ejercicios/08.js
+++ C6 Bucles/ejercicios/08.js
@@ -3,7 +3,13 @@ function esPositivoOInferiorA10(a) {
    // Retorna true si es positivo y menor que 10.
    // Retorna false en caso contrario.
    // Tu código:
+   if (a >= 0 && a < 10) {
+      return true;     
+   }
+   return false;
    
 }
+
+console.log(esPositivoOInferiorA10(0));
 
 module.exports = esPositivoOInferiorA10;
